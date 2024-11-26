dfd2999bb86f737264c598f6191ca472b42b1928
temsisha <40714959+temsisha@users.noreply.github.com>
2024-11-26 18:43:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ function findSongByTitle(documentContent, songTitle) {
                 const cleanSongTitle = cleanText(songTitle);
 
                 // Dodajemo paragrafni sadržaj u trenutnu stranicu
-                //currentPageContent += paragraphContent + '\n';
+                currentPageContent += paragraphContent + '\n';
 
                 // Ako pronađemo naziv pesme, postavljamo oznaku da je pronađena
                 if (cleanParagraphContent.includes(cleanSongTitle)) {
@@ -129,7 +129,7 @@ function findSongByTitle(documentContent, songTitle) {
             if (element.sectionBreak || (element.paragraph && element.paragraph.elements.some(e => e.pageBreak))) {
                 if (songFound) {
                     // Prikazujemo sadržaj cele stranice kada pronađemo pesmu
-                    contentElement.innerHTML = `Song found\n\n${currentPageContent}`;
+                    contentElement.innerHTML = `Song found on page ${pageCount}:\n\n${currentPageContent}`;
                     console.log("----- Document Content End -----");
                     return;
                 }
